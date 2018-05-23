a9f9d89927cb969210d4a1f701d26a28ea4ab510
Jebediah Elliott <jebediahelliott@gmail.com>
2018-05-23 16:46:44 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,6 +2,10 @@ require 'pry'
 
 def prime?(int)
   range = Array.new(1..9)
-binding.pry
-  range
+  range.all? do |num|
+    if int % num != 0 && num != int
+      return true
+      
+      
+      
 end
