64651939f6e0fd2cac74d462d37f5dc5cc467c27
Jebediah Elliott <jebediahelliott@gmail.com>
2018-05-23 18:45:25 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 require 'pry'
 def prime?(int)
-  range = (2..int).to_a
-  binding.pry
+  range = (2...int).to_a
+  #binding.pry
 
   if int < 2
     return false
@@ -9,11 +9,10 @@ def prime?(int)
   if int == 2
     return true
   end
-  x = range.each do |num|
+  range.each do |num|
     if int % num != 0
       return true
     end
   end
   return false
-  x
 end
