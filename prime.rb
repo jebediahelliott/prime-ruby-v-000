08d882ce392cd574184598a071c212225ff207c1
Jebediah Elliott <jebediahelliott@gmail.com>
2018-05-23 16:52:07 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,6 +3,8 @@ require 'pry'
 def prime?(int)
   range = [1,2,3,4,5,6,7,8,9]
   range.all? do |num|
+    if num == int
+      range.delete(int)
     if int % num != 0 && num != int
       return true
     end
